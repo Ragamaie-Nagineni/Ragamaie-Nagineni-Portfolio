5bf1a143eab777d489896c148a16c8f3d6d5879d
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2025-12-22 22:36:00 +0530

    edited extra curricular

--- src/components/Extracurricular.jsx
+++ src/components/Extracurricular.jsx
@@ -3,7 +3,7 @@ import { FaUsers } from "react-icons/fa";
 export default function Extracurricular() {
   return (
     <section id="extracurricular">
-        <h2><FaUsers /> Extracurricular & Volunteering</h2>
+      <h2><FaUsers /> Extracurricular & Volunteering</h2>
       <div className="container">
         <h3>PR Core Member, Epoch Club (Aug 2024 – Apr 2025)</h3>
         <ul>
@@ -12,6 +12,13 @@ export default function Extracurricular() {
           <li>Led promotional campaigns using social media and creative marketing techniques, expanding outreach within the student community.</li>
           <li>Strengthened leadership, communication, and project management skills through hands-on event coordination.</li>
         </ul>
+        <h3>Classical Dancer & BEATRIPPERZ Member  (Aug 2023 – Dec 2025)</h3>
+        <ul>
+          <li>Practicing classical dance and Active member of IIITs dance club BEATRIPPERZ for 3+ years</li>
+          <li>Participated in cultural fests, performances</li>
+          <li>Developed discipline, teamwork, stage presence, and creative expression</li>
+        </ul>
+
       </div>
     </section>
   );
