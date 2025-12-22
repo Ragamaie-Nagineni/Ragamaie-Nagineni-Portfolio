42baeddd4ac7f5fc920532af041f0a763d73c695
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2025-12-22 22:25:48 +0530

    Revert "changed skills css"
    This reverts commit dae2c8d026cacfd7b05310633b7991a06b2efb3c.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,36 +5,30 @@ export default function Skills() {
     <section id="skills">
       <h2><FaTools /> Skills</h2>
 
-      <div className="container skills-layout">
+      <div className="container">
 
-        {/* LEFT COLUMN */}
-        <div className="skills-col">
-          <h3>Programming Languages</h3>
-          <p>Python, C, Java</p>
+        <h3>Programming Languages</h3>
+        <p>Python, C, Java</p>
 
-          <h3>Web Development</h3>
-          <ul>
-            <li>Frontend: HTML, CSS, JavaScript, React</li>
-            <li>Backend: Node.js, Express</li>
-            <li>Version Control: Git, GitHub</li>
-          </ul>
+        <h3>Web Development</h3>
+        <ul>
+          <li>Frontend: HTML, CSS, JavaScript, React</li>
+          <li>Backend: Node.js, Express</li>
+          <li>Version Control: Git, GitHub</li>
+        </ul>
 
-          <h3>Databases</h3>
-          <p>PostgreSQL, MongoDB</p>
-        </div>
+        <h3>Databases</h3>
+        <p>PostgreSQL, MongoDB</p>
 
-        {/* RIGHT COLUMN */}
-        <div className="skills-col">
-          <h3>Data Science & ML</h3>
-          <ul>
-            <li>Pandas, Scikit-learn, Matplotlib</li>
-            <li>Data Analysis, Statistical Modelling</li>
-            <li>ML Algorithms & Evaluation</li>
-          </ul>
+        <h3>Data Science & ML</h3>
+        <ul>
+          <li>Pandas, Scikit-learn, Matplotlib</li>
+          <li>Data Analysis, Statistical Modelling</li>
+          <li>ML Algorithms & Evaluation</li>
+        </ul>
 
-          <h3>Core CS Subjects</h3>
-          <p>DSA, OS, DBMS, OOPS, CN</p>
-        </div>
+        <h3>Core CS Subjects</h3>
+        <p>DSA, OS, DBMS, OOPS, CN</p>
 
       </div>
     </section>
