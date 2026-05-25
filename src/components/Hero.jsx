d00e0519f952f2e690cb081d1959a6fa5d9fa5de
Ragamaie-Nagineni <ragamaie.n@gmail.com>
2026-05-25 21:26:02 +0530

    fixed resume download issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,47 @@
-const BASE = import.meta.env.BASE_URL;
 export default function Hero() {
   return (
-    <section id="hero"  className="hero reveal">
+    <section id="hero" className="hero reveal">
+
       <div className="hero-box">
-        <h1> <span>Ragamaie Nagineni</span></h1>
-        <p>Computer Science Undergraduate • Web Developer • Cloud Computing Enthusiast</p>
+
+        <h1>
+          <span>Ragamaie Nagineni</span>
+        </h1>
+
+        <p>
+          Computer Science Undergraduate • Web Developer • Cloud Computing Enthusiast
+        </p>
 
         <div className="btn-box">
-          <a href="#/" className="btn primary">
-            <span onClick={() => {
-              setTimeout(() => {
-                document.getElementById("projects")?.scrollIntoView({ behavior: "smooth" });
-              }, 100);
-            }}>
-              View Projects
-            </span>
+
+          <a
+            href="#projects"
+            className="btn primary"
+            onClick={(e) => {
+              e.preventDefault();
+
+              document
+                .getElementById("projects")
+                ?.scrollIntoView({
+                  behavior: "smooth",
+                });
+            }}
+          >
+            View Projects
           </a>
 
-          <a href={`${BASE}Resume.pdf`} download className="btn secondary">
+          <a
+            href={`${import.meta.env.BASE_URL}Resume.pdf`}
+            download
+            className="btn secondary"
+          >
             Download Resume
           </a>
+
         </div>
+
       </div>
+
     </section>
   );
 }
